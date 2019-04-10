53743f129febae7642173aa6ca1473099b2de3fe
ThePredatorBG <thepredatorbg@abv.bg>
2019-04-10 18:13:33 +0300

    Contra Launcher form could not be minimized when clicking on the icon in taskbar. Fixed.

--- Form1.cs
+++ Form1.cs
@@ -276,6 +276,21 @@ namespace Contra
         }
         //**********DRAG FORM CODE END**********
 
+        //**********MINIMIZE FORM CODE START**********
+        const int WS_MINIMIZEBOX = 0x20000;
+        const int CS_DBLCLKS = 0x8;
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.Style |= WS_MINIMIZEBOX;
+                cp.ClassStyle |= CS_DBLCLKS;
+                return cp;
+            }
+        }
+        //**********MINIMIZE FORM CODE END**********
+
 
         private void checkInstallDir()
         {
